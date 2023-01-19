53c64641c6cdf04912dfdcbf160193d3dbc14b1b
Iván Pérez <iperero92@gmail.com>
2023-01-19 22:31:21 +0100

    refactor: abstract player

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,17 @@ use sdl2::rect::{Point, Rect};
 use sdl2::image::{self, LoadTexture, InitFlag};
 use std::time::Duration;
 
+#[derive(Debug)]
+struct Player {
+    position: Point,
+    sprite: Rect,
+}
+
 fn render(
     canvas: &mut WindowCanvas,
     color: Color,
 	texture: &Texture,
-    position: Point,
-    sprite: Rect,
+    player: &Player,
     ) -> Result<(),String>{
     canvas.set_draw_color(color);
     canvas.clear();
@@ -22,9 +27,9 @@ fn render(
     let (width, height) = canvas.output_size()?;
 
     // Treat the center of the screen as the (0, 0) coordinate
-    let screen_position = position + Point::new(width as i32 / 2, height as i32 / 2);
-    let screen_rect = Rect::from_center(screen_position, sprite.width(), sprite.height());
-    canvas.copy(texture, sprite, screen_rect)?;
+    let screen_position = player.position + Point::new(width as i32 / 2, height as i32 / 2);
+    let screen_rect = Rect::from_center(screen_position, player.sprite.width(), player.sprite.height());
+    canvas.copy(texture, player.sprite, screen_rect)?;
     
     canvas.present();
     
@@ -46,11 +51,14 @@ fn main() -> Result<(), String> {
     let texture_creator = canvas.texture_creator();
     let texture = texture_creator.load_texture("assets/bardo.png")?;
     
-    let position = Point::new(0, 0);
-    // src position in the spritesheet
     let mut event_pump = sdl_context.event_pump()?;
     let mut i = 0;
     let mut column = 0; let mut row = 0;
+    let mut player = Player {
+        position: Point::new(-30, 55),
+        sprite: Rect::new(column*26, row*36, 26, 36),
+    };
+    
     'running: loop {
         //process input
         for event in event_pump.poll_iter() {
@@ -65,15 +73,10 @@ fn main() -> Result<(), String> {
 
         //Update
         i = (i + 1) % 255;
-        column = (column + 1) % 12;
-        if column == 0 {row = {row + 1} % 8}
-        
-        let sprite = Rect::new(column*26, row*36, 26, 36);
-    
         
         //render
         //
-        render(&mut canvas, Color::RGB(i, 64, 255 - i),&texture,position,sprite)?;
+        render(&mut canvas, Color::RGB(i, 64, 255 - i),&texture,&player)?;
         
         //time management
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
