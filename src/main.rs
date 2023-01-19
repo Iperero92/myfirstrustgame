5f4009fe873b38586450c0726742df734857ba99
Iván Pérez <iperero92@gmail.com>
2023-01-19 22:25:27 +0100

    feat(animation): loop through the sprites

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,27 @@ use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::render::{WindowCanvas, Texture};
-use std::time::Duration;
+use sdl2::rect::{Point, Rect};
 // "self" imports the "image" module itself as well as everything else we listed
 use sdl2::image::{self, LoadTexture, InitFlag};
+use std::time::Duration;
 
-fn render(canvas: &mut WindowCanvas, color: Color, texture: &Texture) -> Result<(),String>{
+fn render(
+    canvas: &mut WindowCanvas,
+    color: Color,
+	texture: &Texture,
+    position: Point,
+    sprite: Rect,
+    ) -> Result<(),String>{
     canvas.set_draw_color(color);
     canvas.clear();
     
-    canvas.copy(texture, None, None)?;
+    let (width, height) = canvas.output_size()?;
+
+    // Treat the center of the screen as the (0, 0) coordinate
+    let screen_position = position + Point::new(width as i32 / 2, height as i32 / 2);
+    let screen_rect = Rect::from_center(screen_position, sprite.width(), sprite.height());
+    canvas.copy(texture, sprite, screen_rect)?;
     
     canvas.present();
     
@@ -34,8 +46,11 @@ fn main() -> Result<(), String> {
     let texture_creator = canvas.texture_creator();
     let texture = texture_creator.load_texture("assets/bardo.png")?;
     
+    let position = Point::new(0, 0);
+    // src position in the spritesheet
     let mut event_pump = sdl_context.event_pump()?;
     let mut i = 0;
+    let mut column = 0; let mut row = 0;
     'running: loop {
         //process input
         for event in event_pump.poll_iter() {
@@ -50,9 +65,15 @@ fn main() -> Result<(), String> {
 
         //Update
         i = (i + 1) % 255;
+        column = (column + 1) % 12;
+        if column == 0 {row = {row + 1} % 8}
+        
+        let sprite = Rect::new(column*26, row*36, 26, 36);
+    
         
         //render
-        render(&mut canvas, Color::RGB(i, 64, 255 - i),&texture);
+        //
+        render(&mut canvas, Color::RGB(i, 64, 255 - i),&texture,position,sprite)?;
         
         //time management
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
